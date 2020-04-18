8f4410a109c1759a6d086be821e00cacb6495d31
mdjacksparrow <45224729+mdjacksparrow@users.noreply.github.com>
2020-04-18 15:52:52 +0530

    Change
    Date changing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const date = new Date(2020, 1, 1, 10);
+const date = new Date(2020, 1, 1, 19);
 
 ReactDOM.render(<App date={date} />, document.getElementById("root"));
